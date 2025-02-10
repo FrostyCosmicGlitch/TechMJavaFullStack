b9b80632072053c5bc57d16a68a26ffaf54f6a94
FrostyCosmicGlitch <128168219+FrostyCosmicGlitch@users.noreply.github.com>
2025-02-10 17:12:40 +0530

    Create Utility.java

---
+++ week2/Premitive_And_Reference/Utility.java
@@ -0,0 +1,8 @@
+import utility.NumberUtils;
+
+class Utility {
+    public static void main(String[] args) {
+        int num = 12345;
+        System.out.println("Length of " + num + " = " + NumberUtils.getLength(num));
+    }
+}
