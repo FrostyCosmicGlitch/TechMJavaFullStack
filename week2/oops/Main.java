be4770f6274eb23b9e5cab16ed9058c2cc078e80
FrostyCosmicGlitch <128168219+FrostyCosmicGlitch@users.noreply.github.com>
2025-02-10 13:12:43 +0530

    Create Main.java

---
+++ week2/oops/Main.java
@@ -0,0 +1,25 @@
+class ElectricCar extends Car {
+    private int batteryRange;
+
+    public ElectricCar(String make, String model, int year, int batteryRange) {
+        super(make, model, year);
+        this.batteryRange = batteryRange;
+    }
+
+    public int getBatteryRange() {
+        return batteryRange;
+    }
+
+    public void setBatteryRange(int batteryRange) {
+        this.batteryRange = batteryRange;
+    }
+
+    public void chargeBattery() {
+        System.out.println("Battery is charging...");
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("ElectricCar engine started silently");
+    }
+}
